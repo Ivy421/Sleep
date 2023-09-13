97743517344cc93559b6c52d456be7ef762d7db5
Ivy <57535126+Ivy421@users.noreply.github.com>
2023-09-13 16:59:39 +0800

    Add files via upload

--- groupfile/GroupFile.py
+++ groupfile/GroupFile.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
-
 
 from nptdms import TdmsFile
 import os, shutil, re, csv
@@ -26,19 +24,22 @@ p_bout= pd.DataFrame()
 group_hour = pd.DataFrame()
 group_avg = pd.DataFrame()
 
+# remove previous file
 for File in os.listdir(fullpath):
         if File.endswith('.xlsm'):
             #print(File)
             excelfile.append(File)
-            
+
+# looping all files to exctract data and group them as dataframe            
 for file in excelfile:       
     print("data extracting from : ",file)
     
     # 6 Cycles concating---
     print('6 Cycles data processing---')
     six_Cycles = pd.read_excel(fullpath+'/'+file, sheet_name = '6 Cycles')
+    # concat target columns
     wt_data = pd.concat([wt_data,six_Cycles.loc[:,'WT':'Cycle-5']],axis = 1)
-    # extract rows unitl the first blank appears
+    # select rows unitl the empty row appears
     wt_data = wt_data.iloc[:wt_data.isnull().any(axis=1).idxmax(),:]
     a5_data = pd.concat([a5_data,six_Cycles.loc[:,'A5':'Cycle-5.1']],axis = 1)
     a5_data = a5_data.iloc[:a5_data.isnull().any(axis=1).idxmax(),:]
@@ -46,6 +47,10 @@ for file in excelfile:
     # sleep bouts concating ---
     sleepbouts = pd.read_excel(fullpath+'/'+file, sheet_name = 'Sleep Bouts')
     print('Sleep Bouts data processing---')
+    
+    # source sheets have no column name,system automatively add column name as 'Unnamed: 0'
+    # since one sheet includes too many contents, 
+    # find the target content by keyword:Data-1
     id_data1 = sleepbouts[sleepbouts["Unnamed: 0"]== "Data-1"].index.tolist()[0]
     sleepbouts = sleepbouts.iloc[id_data1+1:,:]
     id_data1_end = sleepbouts[sleepbouts["Unnamed: 0"]== "Data-1"].index.tolist()[0]
@@ -59,23 +64,26 @@ for file in excelfile:
     # group count data concating ---
     print('groupcount data processing---')
     groupcount = pd.read_excel(fullpath+'/'+file, sheet_name ='GroupCount Data')
+    #extract column index
     hour_hour = list(groupcount).index("Hour-1 After Correction by Hour")
     hour_avg = list(groupcount).index('Hour-1 After Correction by Average')
     group_hour = pd.concat([group_hour,groupcount.iloc[:,hour_hour:hour_hour+3]],axis = 1)
     group_avg = pd.concat([group_avg,groupcount.iloc[:,hour_avg:hour_avg+3]],axis = 1)
 
+# final concating after looping done
 six_cycles_total = pd.concat([wt_data,a5_data],axis = 1)
-
 group_hour.reset_index(drop = True,inplace = True)
 group_avg.reset_index(drop = True,inplace = True)
 groupc_total = pd.concat([group_hour,group_avg],axis = 1)
-print('Grouping DONE!')
 
+# data frame write to sheets
 with pd.ExcelWriter(fullpath +'/groupfile.xlsx', engine = 'xlsxwriter') as writer:
+    print('data to sheet processing ---')
     six_cycles_total.to_excel(writer ,sheet_name = '6 Cycles',index = False ,header = True)
     p_bout.to_excel(writer ,sheet_name = 'Sleep Bouts',index = False ,header = False)
     groupc_total.to_excel(writer ,sheet_name = 'Group Count Data',index = False ,header = True)
     
+    # set column width
     worksheet = writer.sheets['6 Cycles']
     worksheet.set_column("A:Z", 10)
     worksheet = writer.sheets['Sleep Bouts']
@@ -84,16 +92,8 @@ with pd.ExcelWriter(fullpath +'/groupfile.xlsx', engine = 'xlsxwriter') as write
     worksheet.set_column("A:Z", 15)
     
 writer.save()
-writer.close()
-
-
-# In[7]:
-
-
-
-
-
-# In[ ]:
+#writer.close()
+print('Groupfile DONE!')
 
 
 
